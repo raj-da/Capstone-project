f64b4bea2751c0c108166d0d0c13da62a011c987
raj-da <rajafgudeta@gmail.com>
2025-01-09 00:22:49 +0300

    added all api generic views and endpoints .... working version

--- Recipe_Management/api/urls.py
+++ Recipe_Management/api/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('create/', views.CreateRecipe.as_view(), name='create recipe'),
+    path('retrieve/', views.RettriveRecipes.as_view(), name='retrieve recipes'),
+    path('retrieve/<int:pk>/', views.DetailView.as_view(), name='Detail view'),
+    path('update/<int:pk>/', views.UpdateView.as_view(), name='update view'),
+    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete view'),
 ]
--- Recipe_Management/api/views.py
+++ Recipe_Management/api/views.py
@@ -1,5 +1,27 @@
 from rest_framework import generics
+from recipes import models
 from . import serializer
 
 class CreateRecipe(generics.CreateAPIView):
     serializer_class = serializer.RecpieSerializer
+
+
+class RettriveRecipes(generics.ListAPIView):
+    queryset = models.Recipe.objects.all()
+    serializer_class = serializer.RecpieSerializer
+
+
+class DetailView(generics.RetrieveAPIView):
+    queryset = models.Recipe.objects.all()
+    serializer_class = serializer.RecpieSerializer
+
+
+class UpdateView(generics.RetrieveUpdateAPIView):
+    queryset = models.Recipe.objects.all()
+    serializer_class = serializer.RecpieSerializer
+
+
+class DeleteView(generics.RetrieveDestroyAPIView):
+    queryset = models.Recipe.objects.all()
+    serializer_class = serializer.RecpieSerializer
+
